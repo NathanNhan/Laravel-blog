1a811e7a365cb7ecaf2bd947b3177658100a9dac
NathanNhan <trongnhan8150@gmail.com>
2021-09-01 13:30:35 +0700

    update laravel blog

---
+++ app/Http/Controllers/api/blogController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class blogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Blog::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       return Blog::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        return $blog;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        //
+        return $blog->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+        return $blog->delete();
+    }
+
+    //search 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function search( $title)
+    {
+        //
+        return Blog::where('title','like', '%'.$title.'%')->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\blogController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+//public route
+Route::get('blogs', [blogController::class, 'index']);
+Route::get('blogs/{blog}', [blogController::class, 'show']);
+Route::get('blogs/search/{title}', [blogController::class, 'search']);
+//Route::resource('blogs', blogController::class);
+//search 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']],function(){
+    Route::post('blogs', [blogController::class, 'store']);
+    Route::put('blogs/{blog}', [blogController::class, 'update']);
+    Route::delete('blogs/{blog}', [blogController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+
+
+
